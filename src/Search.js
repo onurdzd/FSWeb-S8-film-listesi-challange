e8ffc80ae69d5685847bee959f024fb031d4a45d
onurd <106928484+onurdzd@users.noreply.github.com>
2023-02-14 10:07:12 +0300

    search bar boşsa boş dizi geliyor

--- src/Search.js
+++ src/Search.js
@@ -3,11 +3,15 @@ import React, { useEffect, useState } from "react";
 const Search = ({ setSecilenDizi, data }) => {
   const [aramaSonuc, setAramaSonuc] = useState();
   const handleSearch = (e) => {
-    setAramaSonuc(
-      data.find((item) =>
-        item.name.toLowerCase().includes(e.target.value.toLowerCase())
-      )
-    );
+    if (e.target.value) {
+      setAramaSonuc(
+        data.find((item) =>
+          item.name.toLowerCase().includes(e.target.value.toLowerCase())
+        )
+      );
+    } else {
+      setAramaSonuc("");
+    }
   };
 
   useEffect(() => {
