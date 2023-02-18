6c1293a9d7bfb8befb4fe4786bcb25177201e3ab
onurd <106928484+onurdzd@users.noreply.github.com>
2023-02-18 14:19:46 +0300

    dizi oluştur js eklendi işlevsel hale getirilecek

--- src/Components/DiziOlustur.js
+++ src/Components/DiziOlustur.js
@@ -1,10 +1,24 @@
 import React from "react";
 
 const DiziOlustur = () => {
+  //handlechange eklenip data güncellenecek
   return (
     <div className="diziOlusturMain">
-      <input type="text"></input>
-      <input type="text"></input>
+      <div className="diziOlusturAltDiv">
+        <label>
+          Dizi resim url :
+          <input type="text" placeholder="Dizi resim url adresi gir"></input>
+        </label>
+      </div>
+      <div className="diziOlusturAltDiv">
+        <label>
+          Dizi isim : <input type="text" placeholder="Dizi ismini yaz"></input>
+        </label>
+      </div>
+      <div className="diziOlusturAltDiv2">
+        <button>Ekle</button>
+        <button>Geri Dön</button>
+      </div>
     </div>
   );
 };
